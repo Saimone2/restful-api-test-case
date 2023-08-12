b965ee16bd35f0b797bc9a3b64fcb9a4e64caa23
Saimone2 <hav1802200474@gmail.com>
2023-08-12 11:57:35 +0300

    init

---
+++ src/main/java/com/saimone/restfulapitestcase/RestfulApiTestCaseApplication.java
@@ -0,0 +1,11 @@
+package com.saimone.restfulapitestcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiTestCaseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiTestCaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/saimone/restfulapitestcase/RestfulApiTestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saimone.restfulapitestcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiTestCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
